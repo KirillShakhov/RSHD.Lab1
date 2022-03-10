f2d556a35a4f8b8d56139f82a85905a6f53d254c
kirill <kirill.shakhov14@gmail.com>
2022-03-11 00:24:42 +0300

    fix

--- lab.sql
+++ lab.sql
@@ -26,12 +26,11 @@ declare
     column_name text;
     description text;
 begin
-    for constraint_name, type, table_name, column_name, description in select * from (select pgc.conname,
+    for constraint_name, type, table_name, column_name, description in select pgc.conname,
                                                                            pgc.contype,
                                                                            ccu.table_name,
                                                                            ccu.column_name,
-                                                                           pg_get_constraintdef(pgc.oid, true)
-        from pg_constraint pgc
+                                                                           substring(pg_get_constraintdef(pgc.oid, true) from 'CHECK #"%#"%' for '#') from pg_constraint pgc
         join pg_namespace nsp on nsp.oid = pgc.connamespace
         join pg_class  cls on pgc.conrelid = cls.oid
         left join information_schema.constraint_column_usage ccu
@@ -39,7 +38,7 @@ begin
                   and nsp.nspname = ccu.constraint_schema
         where contype ='c'
         -- and ccu.table_schema = 'public'
-        order by pgc.conname) as FOO loop
+        order by pgc.conname loop
         insert into results (constraint_name, type, table_name, column_name, description)
         values (constraint_name, type, table_name, column_name, description);
     end loop;
