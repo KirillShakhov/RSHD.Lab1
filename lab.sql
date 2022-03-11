044ce0e5388b3bef59af939f6c9f9acbbc510e21
kirill <kirill.shakhov14@gmail.com>
2022-03-11 14:57:26 +0300

    fix

--- lab.sql
+++ lab.sql
@@ -1,52 +1,47 @@
 /* Лабораторная работа №1 */
-select pgc.conname as constraint_name,
-       pgc.contype,
-       ccu.table_name,
-       ccu.column_name,
-       pg_get_constraintdef(pgc.oid, true)
-from pg_constraint pgc
-join pg_namespace nsp on nsp.oid = pgc.connamespace
-join pg_class  cls on pgc.conrelid = cls.oid
-left join information_schema.constraint_column_usage ccu
-          on pgc.conname = ccu.constraint_name
-          and nsp.nspname = ccu.constraint_schema
-where contype ='c'
--- and ccu.table_schema = 'public'
-order by pgc.conname;
 
 /* временные таблица для формирования ответа */
-create temp table if not exists results(number bigserial, constraint_name text, type text, table_name text, column_name text, description text);
+create temp table if not exists results(number bigserial, sc text, constraint_name text, type text, table_name text, column_name text, description text);
+create temp table if not exists params(schemes text);
+insert into params values (:v1);
 
-/* функция */
+/* Анонимный блок */
 do $$
 declare
     constraint_name text;
     type text;
+    sc text;
     table_name text;
     column_name text;
     description text;
 begin
-    for constraint_name, type, table_name, column_name, description in select pgc.conname,
+    for sc, constraint_name, type, table_name, column_name, description in select nsp.nspname,
+                                                                            pgc.conname,
                                                                            pgc.contype,
-                                                                           ccu.table_name,
-                                                                           ccu.column_name,
+                                                                           cls.relname,
+                                                                           cls.relname,
                                                                            substring(pg_get_constraintdef(pgc.oid, true) from 'CHECK #"%#"%' for '#') from pg_constraint pgc
         join pg_namespace nsp on nsp.oid = pgc.connamespace
-        join pg_class  cls on pgc.conrelid = cls.oid
-        left join information_schema.constraint_column_usage ccu
-                  on pgc.conname = ccu.constraint_name
-                  and nsp.nspname = ccu.constraint_schema
+        join pg_class cls on pgc.conrelid = cls.oid
+
+--         left join information_schema.constraint_column_usage ccu
+--                   on pgc.conname = ccu.constraint_name
+--                   and nsp.nspname = ccu.constraint_schema
         where contype ='c'
-        -- and ccu.table_schema = 'public'
-        order by pgc.conname loop
-        insert into results (constraint_name, type, table_name, column_name, description)
-        values (constraint_name, type, table_name, column_name, description);
+        and (select count(schemes) from params where schemes = nsp.nspname) > 0
+        order by pgc.conname
+    loop
+        insert into results (constraint_name, sc, type, table_name, column_name, description)
+        values (constraint_name, sc, type, table_name, column_name, description);
     end loop;
-end;
+end
 $$ language 'plpgsql';
 
-
 /* Просмотр таблицы-ответа info */
+select * from params;
 select * from results;
 
+-- select * from params where (select schemes from params where schemes = 'public') is not null;
+
 drop table results;
+drop table params;
